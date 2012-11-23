d35e7a16302b12a2c58ccd8b53fa7ab6c82e920c
SND\sanshi_cp <SND\sanshi_cp@e17a0e51-4ae3-4d35-97c3-1a29b211df97>
2012-11-23 09:00:05 +0000

    Update Ext.ux.SimplePagingToolbar.js to use Ext.form.NumberField for pagenumber input.
    git-svn-id: https://fineui.svn.codeplex.com/svn@81360 e17a0e51-4ae3-4d35-97c3-1a29b211df97

--- FineUI/js/ux/Ext.ux.SimplePagingToolbar.js
+++ FineUI/js/ux/Ext.ux.SimplePagingToolbar.js
@@ -31,27 +31,32 @@
         initComponent: function() {
             var pagingItems = [this.first = new T.Button({
                 tooltip: this.firstText,
+                overflowText: this.firstText,
                 iconCls: "x-tbar-page-first",
                 disabled: true,
                 handler: this.onClick,
                 scope: this
             }), this.prev = new T.Button({
                 tooltip: this.prevText,
+                overflowText: this.prevText,
                 iconCls: "x-tbar-page-prev",
                 disabled: true,
                 handler: this.onClick,
                 scope: this
             }), '-', this.beforePageText,
-            this.inputItem = new T.Item({
-                height: 18,
-                autoEl: {
-                    tag: "input",
-                    type: "text",
-                    size: "3",
-                    value: "1",
-                    cls: "x-tbar-page-number"
-                }
-            }), this.afterTextItem = new T.TextItem({
+            this.inputItem = new Ext.form.NumberField({
+				cls: 'x-tbar-page-number',
+				allowDecimals: false,
+				allowNegative: false,
+				enableKeyEvents: true,
+				selectOnFocus: true,
+				submitValue: false,
+				listeners: {
+					scope: this,
+					keydown: this.onPagingKeyDown,
+					blur: this.onPagingBlur
+				}
+			}), this.afterTextItem = new T.TextItem({
                 text: String.format(this.afterPageText, 1)
             }), '-', this.next = new T.Button({
                 tooltip: this.nextText,
@@ -69,17 +74,17 @@
 
 
             var userItems = this.items || this.buttons || [];
-            if (this.prependButtons) {
-                this.items = userItems.concat(pagingItems);
-            } else {
-                this.items = pagingItems.concat(userItems);
-            }
-            delete this.buttons;
-            if (this.displayInfo) {
-                this.items.push('->');
-                this.items.push(this.displayItem = new T.TextItem({}));
-            }
-            Ext.PagingToolbar.superclass.initComponent.call(this);
+			if (this.prependButtons) {
+				this.items = userItems.concat(pagingItems);
+			}else{
+				this.items = pagingItems.concat(userItems);
+			}
+			delete this.buttons;
+			if(this.displayInfo){
+				this.items.push('->');
+				this.items.push(this.displayItem = new T.TextItem({}));
+			}
+            Ext.ux.SimplePagingToolbar.superclass.initComponent.call(this);
 
             this.on('afterlayout', this.onFirstLayout, this, { single: true });
         },
@@ -91,14 +96,21 @@
         },
 
         // private
-        onFirstLayout: function(ii) {
-            this.mon(this.inputItem.el, "keydown", this.onPagingKeyDown, this);
-            this.mon(this.inputItem.el, "blur", this.onPagingBlur, this);
-            this.mon(this.inputItem.el, "focus", this.onPagingFocus, this);
-
-            this.field = this.inputItem.el.dom;
-
+        onFirstLayout: function() {
             this.onLoad();
+        },
+		
+		// private
+        onLoad: function() {
+            var d = this.getPageData(), ap = d.activePage, ps = d.pages;
+
+            this.afterTextItem.setText(String.format(this.afterPageText, d.pages));
+            this.inputItem.setValue(ap);
+            this.first.setDisabled(ap == 1);
+            this.prev.setDisabled(ap == 1);
+            this.next.setDisabled(ap == ps);
+            this.last.setDisabled(ap == ps);
+            this.updateInfo();
         },
 
         // private
@@ -118,19 +130,6 @@
         },
 
         // private
-        onLoad: function() {
-            var d = this.getPageData(), ap = d.activePage, ps = d.pages;
-
-            this.afterTextItem.setText(String.format(this.afterPageText, d.pages));
-            this.field.value = ap;
-            this.first.setDisabled(ap == 1);
-            this.prev.setDisabled(ap == 1);
-            this.next.setDisabled(ap == ps);
-            this.last.setDisabled(ap == ps);
-            this.updateInfo();
-        },
-
-        // private
         getPageData: function() {
             return {
                 total: this.recordCount,
@@ -139,55 +138,53 @@
             };
         },
 
-
         // private
         readPage: function(d) {
-            var v = this.field.value, pageNum;
+            var v = this.inputItem.getValue(), pageNum;
             if (!v || isNaN(pageNum = parseInt(v, 10))) {
-                this.field.value = d.activePage;
+                this.inputItem.setValue(d.activePage);
                 return false;
             }
             return pageNum;
         },
 
         onPagingFocus: function() {
-            this.field.select();
+            this.inputItem.select();
         },
 
         //private
         onPagingBlur: function(e) {
-            this.field.value = this.getPageData().activePage;
+            this.inputItem.setValue(this.getPageData().activePage);
         },
 
         // private
-        onPagingKeyDown: function(e) {
-            var k = e.getKey(), d = this.getPageData(), pageNum;
-            if (k == e.RETURN) {
-                e.stopEvent();
-                pageNum = this.readPage(d);
-                if (pageNum !== false) {
-                    pageNum = Math.min(Math.max(1, pageNum), d.pages) - 1;
-
-                    this.onLoadPage(pageNum);
-                }
-            } else if (k == e.HOME || k == e.END) {
-                e.stopEvent();
-                pageNum = k == e.HOME ? 1 : d.pages;
-                this.field.value = pageNum;
-            } else if (k == e.UP || k == e.PAGEUP || k == e.DOWN || k == e.PAGEDOWN) {
-                e.stopEvent();
-                if ((pageNum = this.readPage(d))) {
-                    var increment = e.shiftKey ? 10 : 1;
-                    if (k == e.DOWN || k == e.PAGEDOWN) {
-                        increment *= -1;
-                    }
-                    pageNum += increment;
-                    if (pageNum >= 1 & pageNum <= d.pages) {
-                        this.field.value = pageNum;
-                    }
-                }
-            }
-        },
+		onPagingKeyDown : function(field, e){
+			var k = e.getKey(), d = this.getPageData(), pageNum;
+			if (k == e.RETURN) {
+				e.stopEvent();
+				pageNum = this.readPage(d);
+				if(pageNum !== false){
+					pageNum = Math.min(Math.max(1, pageNum), d.pages) - 1;
+					this.onLoadPage(pageNum);
+				}
+			}else if (k == e.HOME || k == e.END){
+				e.stopEvent();
+				pageNum = k == e.HOME ? 1 : d.pages;
+				field.setValue(pageNum);
+			}else if (k == e.UP || k == e.PAGEUP || k == e.DOWN || k == e.PAGEDOWN){
+				e.stopEvent();
+				if((pageNum = this.readPage(d))){
+					var increment = e.shiftKey ? 10 : 1;
+					if(k == e.DOWN || k == e.PAGEDOWN){
+						increment *= -1;
+					}
+					pageNum += increment;
+					if(pageNum >= 1 & pageNum <= d.pages){
+						field.setValue(pageNum);
+					}
+				}
+			}
+		},
 
 
         // private
@@ -219,7 +216,7 @@
 
         // private
         onDestroy: function() {
-            Ext.PagingToolbar.superclass.onDestroy.call(this);
+            Ext.ux.SimplePagingToolbar.superclass.onDestroy.call(this);
         }
 
     });
--- FineUI/js/ux/ux.js
+++ FineUI/js/ux/ux.js
@@ -441,27 +441,32 @@ Ext.preg('tabclosemenu', Ext.ux.TabCloseMenu);
         initComponent: function() {
             var pagingItems = [this.first = new T.Button({
                 tooltip: this.firstText,
+                overflowText: this.firstText,
                 iconCls: "x-tbar-page-first",
                 disabled: true,
                 handler: this.onClick,
                 scope: this
             }), this.prev = new T.Button({
                 tooltip: this.prevText,
+                overflowText: this.prevText,
                 iconCls: "x-tbar-page-prev",
                 disabled: true,
                 handler: this.onClick,
                 scope: this
             }), '-', this.beforePageText,
-            this.inputItem = new T.Item({
-                height: 18,
-                autoEl: {
-                    tag: "input",
-                    type: "text",
-                    size: "3",
-                    value: "1",
-                    cls: "x-tbar-page-number"
-                }
-            }), this.afterTextItem = new T.TextItem({
+            this.inputItem = new Ext.form.NumberField({
+				cls: 'x-tbar-page-number',
+				allowDecimals: false,
+				allowNegative: false,
+				enableKeyEvents: true,
+				selectOnFocus: true,
+				submitValue: false,
+				listeners: {
+					scope: this,
+					keydown: this.onPagingKeyDown,
+					blur: this.onPagingBlur
+				}
+			}), this.afterTextItem = new T.TextItem({
                 text: String.format(this.afterPageText, 1)
             }), '-', this.next = new T.Button({
                 tooltip: this.nextText,
@@ -479,17 +484,17 @@ Ext.preg('tabclosemenu', Ext.ux.TabCloseMenu);
 
 
             var userItems = this.items || this.buttons || [];
-            if (this.prependButtons) {
-                this.items = userItems.concat(pagingItems);
-            } else {
-                this.items = pagingItems.concat(userItems);
-            }
-            delete this.buttons;
-            if (this.displayInfo) {
-                this.items.push('->');
-                this.items.push(this.displayItem = new T.TextItem({}));
-            }
-            Ext.PagingToolbar.superclass.initComponent.call(this);
+			if (this.prependButtons) {
+				this.items = userItems.concat(pagingItems);
+			}else{
+				this.items = pagingItems.concat(userItems);
+			}
+			delete this.buttons;
+			if(this.displayInfo){
+				this.items.push('->');
+				this.items.push(this.displayItem = new T.TextItem({}));
+			}
+            Ext.ux.SimplePagingToolbar.superclass.initComponent.call(this);
 
             this.on('afterlayout', this.onFirstLayout, this, { single: true });
         },
@@ -501,14 +506,21 @@ Ext.preg('tabclosemenu', Ext.ux.TabCloseMenu);
         },
 
         // private
-        onFirstLayout: function(ii) {
-            this.mon(this.inputItem.el, "keydown", this.onPagingKeyDown, this);
-            this.mon(this.inputItem.el, "blur", this.onPagingBlur, this);
-            this.mon(this.inputItem.el, "focus", this.onPagingFocus, this);
-
-            this.field = this.inputItem.el.dom;
-
+        onFirstLayout: function() {
             this.onLoad();
+        },
+		
+		// private
+        onLoad: function() {
+            var d = this.getPageData(), ap = d.activePage, ps = d.pages;
+
+            this.afterTextItem.setText(String.format(this.afterPageText, d.pages));
+            this.inputItem.setValue(ap);
+            this.first.setDisabled(ap == 1);
+            this.prev.setDisabled(ap == 1);
+            this.next.setDisabled(ap == ps);
+            this.last.setDisabled(ap == ps);
+            this.updateInfo();
         },
 
         // private
@@ -528,19 +540,6 @@ Ext.preg('tabclosemenu', Ext.ux.TabCloseMenu);
         },
 
         // private
-        onLoad: function() {
-            var d = this.getPageData(), ap = d.activePage, ps = d.pages;
-
-            this.afterTextItem.setText(String.format(this.afterPageText, d.pages));
-            this.field.value = ap;
-            this.first.setDisabled(ap == 1);
-            this.prev.setDisabled(ap == 1);
-            this.next.setDisabled(ap == ps);
-            this.last.setDisabled(ap == ps);
-            this.updateInfo();
-        },
-
-        // private
         getPageData: function() {
             return {
                 total: this.recordCount,
@@ -549,55 +548,53 @@ Ext.preg('tabclosemenu', Ext.ux.TabCloseMenu);
             };
         },
 
-
         // private
         readPage: function(d) {
-            var v = this.field.value, pageNum;
+            var v = this.inputItem.getValue(), pageNum;
             if (!v || isNaN(pageNum = parseInt(v, 10))) {
-                this.field.value = d.activePage;
+                this.inputItem.setValue(d.activePage);
                 return false;
             }
             return pageNum;
         },
 
         onPagingFocus: function() {
-            this.field.select();
+            this.inputItem.select();
         },
 
         //private
         onPagingBlur: function(e) {
-            this.field.value = this.getPageData().activePage;
+            this.inputItem.setValue(this.getPageData().activePage);
         },
 
         // private
-        onPagingKeyDown: function(e) {
-            var k = e.getKey(), d = this.getPageData(), pageNum;
-            if (k == e.RETURN) {
-                e.stopEvent();
-                pageNum = this.readPage(d);
-                if (pageNum !== false) {
-                    pageNum = Math.min(Math.max(1, pageNum), d.pages) - 1;
-
-                    this.onLoadPage(pageNum);
-                }
-            } else if (k == e.HOME || k == e.END) {
-                e.stopEvent();
-                pageNum = k == e.HOME ? 1 : d.pages;
-                this.field.value = pageNum;
-            } else if (k == e.UP || k == e.PAGEUP || k == e.DOWN || k == e.PAGEDOWN) {
-                e.stopEvent();
-                if ((pageNum = this.readPage(d))) {
-                    var increment = e.shiftKey ? 10 : 1;
-                    if (k == e.DOWN || k == e.PAGEDOWN) {
-                        increment *= -1;
-                    }
-                    pageNum += increment;
-                    if (pageNum >= 1 & pageNum <= d.pages) {
-                        this.field.value = pageNum;
-                    }
-                }
-            }
-        },
+		onPagingKeyDown : function(field, e){
+			var k = e.getKey(), d = this.getPageData(), pageNum;
+			if (k == e.RETURN) {
+				e.stopEvent();
+				pageNum = this.readPage(d);
+				if(pageNum !== false){
+					pageNum = Math.min(Math.max(1, pageNum), d.pages) - 1;
+					this.onLoadPage(pageNum);
+				}
+			}else if (k == e.HOME || k == e.END){
+				e.stopEvent();
+				pageNum = k == e.HOME ? 1 : d.pages;
+				field.setValue(pageNum);
+			}else if (k == e.UP || k == e.PAGEUP || k == e.DOWN || k == e.PAGEDOWN){
+				e.stopEvent();
+				if((pageNum = this.readPage(d))){
+					var increment = e.shiftKey ? 10 : 1;
+					if(k == e.DOWN || k == e.PAGEDOWN){
+						increment *= -1;
+					}
+					pageNum += increment;
+					if(pageNum >= 1 & pageNum <= d.pages){
+						field.setValue(pageNum);
+					}
+				}
+			}
+		},
 
 
         // private
@@ -629,7 +626,7 @@ Ext.preg('tabclosemenu', Ext.ux.TabCloseMenu);
 
         // private
         onDestroy: function() {
-            Ext.PagingToolbar.superclass.onDestroy.call(this);
+            Ext.ux.SimplePagingToolbar.superclass.onDestroy.call(this);
         }
 
     });
