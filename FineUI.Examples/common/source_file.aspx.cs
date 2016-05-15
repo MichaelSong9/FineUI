6363f945d066e012051be004d18e4f1831e50e9e
SND\sanshi_cp <SND\sanshi_cp@e17a0e51-4ae3-4d35-97c3-1a29b211df97>
2016-05-15 03:47:33 +0000

    git-svn-id: https://fineui.svn.codeplex.com/svn@98076 e17a0e51-4ae3-4d35-97c3-1a29b211df97

--- FineUI.Examples/common/source_file.aspx.cs
+++ FineUI.Examples/common/source_file.aspx.cs
@@ -13,7 +13,32 @@ namespace FineUI.Examples
         {
             if (!IsPostBack)
             {
-                string file = Request.QueryString["file"];
+                string file = Request.QueryString["file"].ToLower();
+
+                // 不是网站根目录下的文件
+                if (!UnderRootPath(file))
+                {
+                    return;
+                }
+
+
+                // 不允许下载文件的目录
+                string basePath = GetBasePath(file);
+                List<string> disallowPaths = new List<string> { "bin", "obj", "upload", "res", "Properties" };
+                if (disallowPaths.Contains(basePath))
+                {
+                    return;
+                }
+
+
+                // 只能下载指定类型文件
+                string fileType = GetFileType(file);
+                List<string> allowFileTypes = new List<string> { "aspx", "ascx", "master", "ashx", "cs", "xml" };
+                if (!allowFileTypes.Contains(fileType))
+                {
+                    return;
+                }
+            
                 
                 string content = File.ReadAllText(Server.MapPath(file));
 
@@ -49,5 +74,42 @@ namespace FineUI.Examples
 
             }
         }
+
+        private bool UnderRootPath(string fileName)
+        {
+            string filePath = Server.MapPath(fileName);
+            string rootPath = Server.MapPath("~/");
+
+            return filePath.StartsWith(rootPath);
+        }
+
+        private string GetBasePath(string fileName)
+        {
+            string filePath = Server.MapPath(fileName);
+            string rootPath = Server.MapPath("~/");
+
+            string basePath = filePath.Substring(rootPath.Length);
+            int slashIndex = basePath.IndexOf("\\");
+            if (slashIndex >= 0)
+            {
+                basePath = basePath.Substring(0, slashIndex);
+            }
+
+            return basePath;
+        }
+
+        private string GetFileType(string fileName)
+        {
+            string fileType = String.Empty;
+
+            int lastDotIndex = fileName.ToLower().LastIndexOf(".");
+            if (lastDotIndex >= 0)
+            {
+                fileType = fileName.Substring(lastDotIndex + 1);
+            }
+
+            return fileType;
+        }
+
     }
 }
