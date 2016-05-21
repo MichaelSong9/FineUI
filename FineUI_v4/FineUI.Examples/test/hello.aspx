767a41acf508d7c527d8dbad7f855efea7318171
SND\sanshi_cp <SND\sanshi_cp@e17a0e51-4ae3-4d35-97c3-1a29b211df97>
2016-05-21 12:15:02 +0000

    git-svn-id: https://fineui.svn.codeplex.com/svn@98082 e17a0e51-4ae3-4d35-97c3-1a29b211df97

---
+++ FineUI_v4/FineUI.Examples/test/hello.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="True" CodeBehind="hello.aspx.cs" Inherits="EmptyProjectNet20.hello" %>
+
+<!DOCTYPE html>
+<html>
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <f:PageManager ID="PageManager1" runat="server" />
+        <f:Grid ID="Grid1" runat="server" Title="Grid" ShowHeader="false" ShowBorder="false"
+            DataKeyNames="编号,识别" EnableRowDoubleClickEvent="true" OnRowDoubleClick="Grid1_RowDoubleClick">
+            <Columns>
+                <f:BoundField DataField="编号" Width="70px" HeaderText="编号" ExpandUnusedSpace="true"
+                    ID="ctl02" ColumnID="Window1_RegionPanel1_Region1_Panel2_Grid1_ctl02">
+                </f:BoundField>
+            </Columns>
+        </f:Grid>
+    </form>
+</body>
+</html>
---
+++ FineUI_v4/FineUI.Examples/test/hello.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using FineUI;
+using System.Data;
+
+namespace EmptyProjectNet20
+{
+    public partial class hello : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Grid1.DataSource = BulidDataSet();
+                Grid1.DataBind();
+            }
+        }
+
+
+        public DataSet BulidDataSet()
+        {
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+            DataColumn col = new DataColumn("编号", typeof(string));
+            col.MaxLength = 20;
+            dt.Columns.Add(col);                   //注意是dt.columns 然后ADD
+            DataColumn col1 = new DataColumn("识别", typeof(int));
+            col.MaxLength = 20;
+            dt.Columns.Add(col1);
+            DataRow rw = dt.NewRow();          //给DT增加一个新行对象
+            rw["编号"] = "1";
+            rw["识别"] = 26;
+            dt.Rows.Add(rw);                         //   dt.Rows.Add(dt.NewRow());   增加一个空
+            DataRow rw1 = dt.NewRow();
+            rw1["编号"] = "2";
+            rw1["识别"] = 26;
+            dt.Rows.Add(rw1);
+            DataRow rw2 = dt.NewRow();
+            rw2["编号"] = "3";
+            rw2["识别"] = 26;
+            dt.Rows.Add(rw2);
+            ds.Tables.Add(dt);
+            return ds;
+        }
+
+        protected void Grid1_RowDoubleClick(object sender, GridRowClickEventArgs e)
+        {
+            Grid1.Rows.RemoveAt(e.RowIndex);
+        }
+    }
+}
